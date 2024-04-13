132f1f9412dcd8610fe4e4e8754d0e3b7ed5afc9
Khushi Rajpurohit <rajpurohitkhushi36@gmail.com>
2024-04-13 20:59:04 +0530

    dropdown of header

---
+++ src/app/components/dropdown.js
@@ -0,0 +1,35 @@
+
+import { useState } from "react";
+import "./dropdown.css"
+
+const ddArr = ["Clothes", "Shoes", "Electronics"];
+
+export default function DropDown() {
+    let [isOpen, setIsOpen] = useState(false)
+
+    const toggle = function () {
+        setIsOpen(!isOpen)
+    }
+
+
+    return (
+
+        <div className="dropdown">
+            <button className="toggleButton" onClick={toggle}> Products</button>
+           
+            {isOpen && (
+                 <ul className="dropdownMenu">
+                {
+                    ddArr.map(ddItems =>
+
+                        <li>{ddItems}</li>
+
+                    )
+                }
+                </ul>
+            )}
+         
+
+        </div >
+    )
+}
