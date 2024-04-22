3d27490d4013efef88817f61b3fed7e74227d9ec
Khushi Rajpurohit <rajpurohitkhushi36@gmail.com>
2024-04-22 10:07:52 +0530

    modified dropdown,category section and data fetching method from api

---
+++ src/app/data/categoriesDataProvider.js
@@ -0,0 +1,20 @@
+"use client"
+import React, { createContext } from 'react';
+import { useFetchData } from './fetchData'; // The custom hook
+
+const DataContext = createContext();
+
+export default function CategoryDataProvider({ children }) {
+  const { products } = useFetchData("https://api.escuelajs.co/api/v1/categories");
+  console.log("here is data hh",products)
+ 
+
+ 
+
+  return <DataContext.Provider value={products}>{children}</DataContext.Provider>;
+}
+
+export function useData() {
+  return React.useContext(DataContext);
+  
+}
